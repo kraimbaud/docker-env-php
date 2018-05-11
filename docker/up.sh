0e9fb80a4fcf11f473ed49cf7e781809792a28f0
Kevin R <kevin.raimbaud@geoks.fr>
2018-05-11 16:03:26 +0200

    Fix conf php

--- docker/up.sh
+++ docker/up.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-file='.env'
+file=.env
+php_ini=php/config/php.ini
+www_conf=php/config/www.conf
 
-if [ -a $file ]; then
-    echo "$file file exists."
-else
+#############################################
+if [ ! -a $file ]; then
     cp .env.dist $file
     echo "$file file has been created"
+fi
+
+#############################################
+if [ ! -f $php_ini ]; then
+    cp php/php.ini $php_ini
+    echo "php.ini has been created"
+fi
+
+#############################################
+if [ ! -f $www_conf ]; then
+    cp php/www.conf $www_conf
+    echo "www.conf has been created"
 fi
